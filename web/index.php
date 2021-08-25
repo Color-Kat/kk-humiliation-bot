7c0b5b619e0c1f82e6bed83aabe48b68e09f70cb
Color-Kat <katik7979@gmail.com>
2021-08-25 20:26:23 +0300

    confirm code

--- web/index.php
+++ web/index.php
@@ -25,7 +25,7 @@ $app->post('/bot', function () use ($app) {
 
     switch ($data->type) {
         case 'confirmation':
-            return getenv('VK_SECRET_TOKEN');
+            return getenv('VK_CONFIRMATION_CODE');
 
         case 'message_new':
             return 'lox';

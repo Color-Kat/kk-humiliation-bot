ccbafcbeb37d65e0ef64a47c0612a59bbbd3f9f5
Color-Kat <katik7979@gmail.com>
2021-08-25 20:22:54 +0300

    get post data from vk

--- web/index.php
+++ web/index.php
@@ -7,19 +7,35 @@ $app['debug'] = true;
 
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
+    'monolog.logfile' => 'php://stderr',
 ));
+$app->get('/', function () use ($app) {
+    return 'heroku is the bad hosting';
+});
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
+$app->post('/bot', function () use ($app) {
+    $data = json_decode(file_get_contents('php://input'));
 
-// Our web handlers
+    if (!$data) return 'nioh';
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+    if (
+        $data->secret !== getenv('VK_SECRET_TOKEN') &&
+        $data->type !== 'confirmation'
+    ) return 'nioh';
+
+    switch ($data->type) {
+        case 'confirmation':
+            return getenv('VK_SECRET_TOKEN');
+
+        case 'message_new':
+            return 'lox';
+
+        default:
+            # code...
+            break;
+    }
+
+    return 'nioh';
 });
 
 $app->run();

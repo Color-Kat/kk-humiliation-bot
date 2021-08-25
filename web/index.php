ee32afe1d80e4bb8b1b13876474af5e6a830f506
Color-Kat <katik7979@gmail.com>
2021-08-25 22:26:39 +0300

    more variants

--- web/index.php
+++ web/index.php
@@ -70,12 +70,33 @@ $app->post('/bot', function () use ($app) {
                 'высер',
                 'сцыкун',
                 'гад ползучий',
-                // '',
+                'скупердяй',
+                'жадоба',
+                'алочный',
+                'тупой',
+                'глупый',
+                'идиот',
+                'даун',
+                'гей',
+                'трансгендер',
+                'фашист',
+                'ОЛЕГ',
+                'перфаратор',
+                'паразит',
+                'предатель',
+                'горе',
+                '',
+                '',
+                '',
+                '',
             ];
 
             $random_insult_number = rand(0, count($insults));
 
-            $request_params['message'] = 'ты ' . $insults[$random_insult_number];
+            $request_params['message'] = array_rand([
+                'ты',
+                'вы'
+            ]) . ' ' . $insults[$random_insult_number];
 
 
             // if ты or вы in message - send нет, ты
@@ -85,14 +106,42 @@ $app->post('/bot', function () use ($app) {
                 stripos($data->object->body, 'no') !== false ||
                 stripos($data->object->body, 'иди') !== false ||
                 stripos($data->object->body, 'вы') !== false))
-                $request_params['message'] = 'нет, ты';
+                $request_params['message'] = array_rand([
+                    'нет, ты',
+                    'кто обзывается, тот сам так называется!',
+                    'сам такой',
+                    'иди к черту!',
+                    'не обижай меня',
+                    'неа)',
+                    'ты-тыт-тыты-тыт-ыты-тыт-ыты-ыт',
+                    'гыыы',
+                    'нет, вы',
+                    'нет нет нееееет',
+                    'noo',
+                    'nope',
+                ]);
 
             if ((stripos($data->object->body, 'да') !== false ||
                 stripos($data->object->body, 'ага') !== false ||
                 stripos($data->object->body, 'конечно') !== false ||
                 stripos($data->object->body, 'согласен') !== false ||
                 stripos($data->object->body, 'точно') !== false))
-                $request_params['message'] = 'Вот, то-то же';
+                $request_params['message'] = array_rand([
+                    'вот, то-то же',
+                    'ага',
+                    'конечно',
+                    'я не сомневался',
+                    'сто пудов',
+                    'да',
+                    'так точно',
+                    'правильно',
+                    'вот',
+                    'воооот',
+                    'верно',
+                    'я так и думал',
+                    'да',
+                    'ага',
+                ]);
 
             file_get_contents('https://api.vk.com/method/messages.send?' . http_build_query($request_params));
 
